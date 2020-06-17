e044f8d70debd401fc1e02258b6b05856e1a4c2b
MaxonDun <65676752+MaxonDun@users.noreply.github.com>
2020-06-17 19:57:40 +0300

    update

--- database.py
+++ database.py
@@ -1,15 +1,12 @@
 import psycopg2
 import xlwt
 
-
 conn = psycopg2.connect(dbname='rainbow_database',
                         user='unicorn_user',
                         password='magical_password',
                         host='5.101.50.80',
                         port='5432')
 cursor = conn.cursor()
-cursor.execute('SELECT fssp_person FROM fssp')
-record = cursor.fetchall()
 i = 0
 wb = xlwt.Workbook()
 ws = wb.add_sheet('sheet1', cell_overwrite_ok=True)
@@ -18,15 +15,18 @@ ws.write(0, 1, 'Фамилия')
 ws.write(0, 2, 'Имя')
 ws.write(0, 3, 'Отчество')
 
-while i < len(record) :
-    cursor.execute(f'SELECT surname,firstname,secondname FROM persons WHERE id={record[i][0]}')
-    person = cursor.fetchall()
-    print(record[i][0], person[0][0], person[0][1], person[0][2])
-    ws.write(i + 1, 0, record[i][0])
-    ws.write(i + 1, 1, person[0][0])
-    ws.write(i + 1, 2, person[0][1])
-    ws.write(i + 1, 3, person[0][2])
+cursor.execute(f'SELECT fssp.fssp_person, persons.firstname, persons.secondname, '
+               f'persons.surname FROM persons JOIN fssp '
+               f'ON persons.id = fssp.fssp_person '
+               f'ORDER BY fssp.fssp_person')
+person = cursor.fetchall()
+print(person)
+
+while i < len(person):
+    ws.write(i + 1, 0, person[i][0])
+    ws.write(i + 1, 1, person[i][3])
+    ws.write(i + 1, 2, person[i][1])
+    ws.write(i + 1, 3, person[i][2])
     i += 1
     wb.save('my_data.xls')
 conn.close()
-
